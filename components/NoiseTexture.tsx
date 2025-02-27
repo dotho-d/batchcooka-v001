a6d307779e1e1cd6eb2e10262a773d5f16602985
Dotho <dotho.d@influxsocial.org>
2025-02-27 10:17:41 +0400

    Update NoiseTexture.tsx

--- components/NoiseTexture.tsx
+++ components/NoiseTexture.tsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
 
+interface NoiseTextureProps {
+  opacity?: number;
+}
+
+const NoiseTexture: React.FC<NoiseTextureProps> = ({ opacity = 1 }) => {
+  return (
+    <View style={styles.container} pointerEvents="none">
+      <Image
+        source={require("../../assets/images/home_screen_bg.png")}
+        style={[styles.noiseImage, { opacity }]}
+        resizeMode="cover"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1,
+  },
+  noiseImage: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default NoiseTexture;
