c24dd47ef261401f562daba52d583e3897cd9d2a
Dotho <dotho.d@influxsocial.org>
2025-02-28 09:02:15 +0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -91,7 +91,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
-    paddingTop: "15%",
+    paddingTop: "25%",
     paddingHorizontal: 24,
     zIndex: 3,
   },
